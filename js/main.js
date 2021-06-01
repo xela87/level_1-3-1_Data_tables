667bbaa114399aac30f3a78a2908166befbfbd40
Oleksii Khokhlov <xoxlov87@gmail.com>
2021-06-01 18:20:52 +0300

    task done with minimal functional

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 function DataTable(config, data) {
     let tableArea = document.querySelector(config.parent)
     let table = document.createElement('table')
+
+    // Create head of table
     let tableHeadBlock = document.createElement('thead')
     let headRow = document.createElement('tr')
-
     for (let i = 0; i <= config.columns.length; i++) {
         let th = document.createElement('th');
         i === 0 ? th.innerText = '№' : th.innerText = config.columns[i - 1].title;
@@ -12,13 +13,14 @@ function DataTable(config, data) {
     tableHeadBlock.appendChild(headRow)
     table.appendChild(tableHeadBlock)
 
+    // Create body of table
     let tableBodyBlock = document.createElement('tbody')
     for (let i = 0; i < data.length; i++) {
         let tableRow = document.createElement('tr')
         tableRow.insertCell().innerText = `${i + 1}`
-        tableRow.insertCell().innerText = `${data[i].name}`
-        tableRow.insertCell().innerText = `${data[i].surname}`
-        tableRow.insertCell().innerText = `${data[i].age}`
+        for (let j = 0; j < config.columns.length; j++) {
+            tableRow.insertCell().innerText = `${data[i][config1.columns[j].field]}`
+        }
         if (i % 2 !== 0) {
             tableRow.classList.add('dark-row');
         }
@@ -28,6 +30,7 @@ function DataTable(config, data) {
     tableArea.appendChild(table)
 }
 
+    // Configuration of table
 const config1 = {
     parent: '#usersTable',
     columns: [
@@ -37,6 +40,7 @@ const config1 = {
     ]
 };
 
+    // Data
 const users = [
     {id: 30050, name: 'Василь', surname: 'Петров', age: 12},
     {id: 30051, name: 'Петро', surname: 'Васильєв', age: 15},
@@ -46,6 +50,7 @@ const users = [
     {id: 30055, name: 'Катерина', surname: 'Сковорода', age: 29},
 ];
 
+    // Create table with Tabulator library
 let table = new Tabulator("#usersTable1", {
     data: users,
     columns: config1.columns,
@@ -53,4 +58,5 @@ let table = new Tabulator("#usersTable1", {
 });
 table.addColumn({title: '№', field: 'number', formatter: 'rownum'}, true)
 
+    // Create my table
 DataTable(config1, users);
